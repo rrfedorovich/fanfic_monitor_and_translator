ee3ddf9502b768b96043581ce0b4786581cbc71b
rrfedorovich <rrfedorovich@gmail.com>
2026-01-19 10:42:41 +0300

    Update docs for CSV handler

--- src/modules/csv_handler/csv_handler.py
+++ src/modules/csv_handler/csv_handler.py
@@ -9,14 +9,21 @@ class CSVHandler:
         self.file_path: str = file_path
 
     def read_csv(self) -> List[List[str]]:
-        """Читает CSV-файл и возвращает список объектов Fanfic."""
+        """
+        Читает CSV-файл и возвращает список записей для фанфиков.
+        Каждая запись на самом деле список значений для фанфика.
+        """
         with open(self.file_path, mode="r", encoding="utf-8") as file:
             reader = csv.reader(file, delimiter=";")
             field_names = next(reader)
             return [row for row in reader]
 
     def write_csv(self, fanfics: List[List[str]]):
-        """Записывает данные в CSV-файл."""
+        """
+        Записывает данные в CSV-файл.
+        Входная переменная: список записей для фанфиков,
+                            где каждая запись - список значений фанфика.
+        """
         with open(self.file_path, mode="r", encoding="utf-8") as file:
             reader = csv.reader(file, delimiter=";")
             field_names = next(reader)
